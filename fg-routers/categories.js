16b99eb4b303196da560a456090e5d80725dcc06
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-08-27 20:36:12 +0530

    CREATED BY BISWA

--- fg-routers/categories.js
+++ fg-routers/categories.js
@@ -1,3 +1,10 @@
+/*
+CREATED BY
+
+NAME: BISWARUP BHATTACHARJEE
+PH NO.: 6290272740
+EMAIL: bbiswa471@gmail.com
+*/
 const {Category} = require('../fg-models/category');
 const express = require('express');
 const router = express.Router();
